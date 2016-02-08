5353c5e70592202ec3a953c8e46897ae43793b76
Heike Hofmann <hofmann@iastate.edu>
2016-02-08 12:56:18 +1100

    adding the lines to the paper

--- Code/email_lineups.R
+++ Code/email_lineups.R
@@ -4,30 +4,6 @@
 # H_0: within a department, all emails are random
 # H_1: "", there is a strucutre. 
 
-
-
-
-library(geomnet)
-data("email")
-
-head(email$nodes)
-
-email_net <- merge(subset(email$edges, nrecipients < 54), email$nodes, by.x = "From", by.y = "label", all = T)
-
-
-emailm3 <- create_net_lineup(email_net, to_id = "to", m =3)
-ggplot(data = emailm3$lineup_data, aes(from_id = From, to_id = to)) + 
-  geom_net(ealpha = .5, fiteach = TRUE, colour = 'black') + theme_net() + facet_wrap(~group)
-
-emailm6 <- create_net_lineup(email_net, to_id = "to", m =6)
-ggplot(data = emailm6$lineup_data, aes(from_id = From, to_id = to)) + 
-  geom_net(ealpha = .5, fiteach = TRUE, colour = 'black') + theme_net() + facet_wrap(~group)
-
-emailm9 <- create_net_lineup(email_net, to_id = "to", m =9)
-ggplot(data = emailm9$lineup_data, aes(from_id = From, to_id = to)) + 
-  geom_net(ealpha = .5, fiteach = TRUE, colour = 'black') + theme_net() + facet_wrap(~group)
-
-
 create_net_lineup <- function(net_data, to_id, m){
   N <- nrow(net_data)
   nullplots <- NULL
@@ -40,4 +16,31 @@ create_net_lineup <- function(net_data, to_id, m){
   lineup_data$group <- rep(sample.int(m), each=N)
   return(list(lineup_data = lineup_data, data_plot = lineup_data$group[1]))
 }
+
+
+
+library(geomnet)
+data("email")
+
+head(email$nodes)
+
+email_net <- merge(subset(email$edges, nrecipients < 54), email$nodes, by.x = "From", by.y = "label", all = T)
+
+# create lineups in different sizes
+emailm3 <- create_net_lineup(email_net, to_id = "to", m =3)
+emailm6 <- create_net_lineup(email_net, to_id = "to", m =6)
+emailm9 <- create_net_lineup(email_net, to_id = "to", m =9)
+
+# we need to set the seed to make the examples reproducible.
+# One way to get a replicate for graphs might be to have some lineups with the same data but different seeds
+ggplot(data = emailm3$lineup_data, aes(from_id = From, to_id = to)) + 
+  geom_net(ealpha = .5, fiteach = TRUE, colour = 'black') + theme_net() + facet_wrap(~group)
+
+ggplot(data = emailm6$lineup_data, aes(from_id = From, to_id = to)) + 
+  geom_net(ealpha = .5, fiteach = TRUE, colour = 'black') + theme_net() + facet_wrap(~group)
+
+ggplot(data = emailm9$lineup_data, aes(from_id = From, to_id = to)) + 
+  geom_net(ealpha = .5, fiteach = TRUE, colour = 'black') + theme_net() + facet_wrap(~group)
+
+
 
