d7623289a43fa1f753b513af8bf40f8fbda44493
Samantha Tyner <sctyner@iastate.edu>
2016-04-29 14:59:29 -0500

    add mini-experiment materials

---
+++ GGExperimentApr28/experiment_res_analysis.R
@@ -0,0 +1,21 @@
+# analyzing results of mini-experiment 
+
+setwd("GGExperimentApr28")
+
+library(readxl)
+library(dplyr)
+library(magrittr)
+response <- read_excel("responses_GGExpApr28.xlsx")
+response %<>% mutate(correct = as.numeric(Answer == ChosenLU))
+response %>% group_by(Respondent) %>% 
+  dplyr::summarise(perc_right = sum(na.omit(correct))/20)
+
+library(ggplot2)
+
+ggplot(data = response, aes(x = as.factor(ChosenLU), fill = as.factor(correct))) + 
+  geom_bar() +
+  facet_wrap(~Lineup, scales = 'free')
+
+response %>% group_by(Lineup) %>%
+  dplyr::summarise(perc_guessed = sum(na.omit(correct))/11, tot_resp = length(unique(ChosenLU)))
+
