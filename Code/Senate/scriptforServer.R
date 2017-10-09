251af016b90f8c3c9194bce17184ac0b0b861ff4
sctyner <sctyner@iastate.edu>
2017-10-09 16:58:40 -0500

    couple of intro blurbs: SAOMs and visual inference

--- Code/Senate/scriptforServer.R
+++ Code/Senate/scriptforServer.R
@@ -9,7 +9,7 @@ myalg <- sienaAlgorithmCreate( projname = Sys.time() , n3 = 1000)
 #Senjtt_s <- includeEffects(SenBasic, "jumpXTransTrip", include = TRUE, type = "eval", interaction1 = "sex", character = TRUE)
 # SeninIsD <- includeEffects(SenBasic, "inIsDegree", include = TRUE, type = "eval", interaction1 = "", character = TRUE)
 # Sensame_p <- includeEffects(SenBasic, "sameX", include = TRUE, type = "eval", interaction1 = "party", character = TRUE)
-Senstt_b <- includeEffects(SenBasic, "simXTransTrip", include = TRUE, type = "eval", interaction1 = "bills", character = TRUE)
+# Senstt_b <- includeEffects(SenBasic, "simXTransTrip", include = TRUE, type = "eval", interaction1 = "bills", character = TRUE)
 # Sensimre_p <- includeEffects(SenBasic, "simRecipX", include = TRUE, type = "eval", interaction1 = "party", character = TRUE)
 # Sentt_p <- includeEffects(SenBasic, "transTrip", include = TRUE, type = "eval", interaction1 = "", character = TRUE)
 Senstt_p <- includeEffects(SenBasic, "sameXTransTrip", include = TRUE, type = "eval", interaction1 = "party", character = TRUE)
@@ -27,32 +27,12 @@ get_effects_dist <- function(dat, struct, N){
                       "maxConv")
   for (i in 1:N){
     fits <- siena07(myalg, data = dat, effects = struct, returnDeps = FALSE,
-                    batch=TRUE, verbose = FALSE, silent = TRUE)
+                    batch=TRUE, verbose = FALSE, silent = TRUE, useCluster = TRUE, nbrNodes = 3, 
+                    initC = TRUE, clusterIter = FALSE)
     df.ests[i,] <- c(fits$rate, fits$theta, sqrt(diag(fits$covtheta)), fits$tconv.max)
   }
   return(df.ests)
 }
-
-simttbFitsSenate <- get_effects_dist(dat = senateSiena, struct = Senstt_b, N = 100)
-write.csv(simttbFitsSenate, "simttbModelFitsSenate100.csv")
-simttbFitsSenate <- get_effects_dist(dat = senateSiena, struct = Senstt_b, N = 100)
-write.csv(simttbFitsSenate, "simttbModelFitsSenate200.csv")
-simttbFitsSenate <- get_effects_dist(dat = senateSiena, struct = Senstt_b, N = 100)
-write.csv(simttbFitsSenate, "simttbModelFitsSenate300.csv")
-simttbFitsSenate <- get_effects_dist(dat = senateSiena, struct = Senstt_b, N = 100)
-write.csv(simttbFitsSenate, "simttbModelFitsSenate400.csv")
-simttbFitsSenate <- get_effects_dist(dat = senateSiena, struct = Senstt_b, N = 100)
-write.csv(simttbFitsSenate, "simttbModelFitsSenate500.csv")
-simttbFitsSenate <- get_effects_dist(dat = senateSiena, struct = Senstt_b, N = 100)
-write.csv(simttbFitsSenate, "simttbModelFitsSenate600.csv")
-simttbFitsSenate <- get_effects_dist(dat = senateSiena, struct = Senstt_b, N = 100)
-write.csv(simttbFitsSenate, "simttbModelFitsSenate700.csv")
-simttbFitsSenate <- get_effects_dist(dat = senateSiena, struct = Senstt_b, N = 100)
-write.csv(simttbFitsSenate, "simttbModelFitsSenate800.csv")
-simttbFitsSenate <- get_effects_dist(dat = senateSiena, struct = Senstt_b, N = 100)
-write.csv(simttbFitsSenate, "simttbModelFitsSenate900.csv")
-simttbFitsSenate <- get_effects_dist(dat = senateSiena, struct = Senstt_b, N = 100)
-write.csv(simttbFitsSenate, "simttbModelFitsSenate1000.csv")
 
 samettpFitsSenate <-get_effects_dist(dat = senateSiena, struct = Senstt_p, N = 100)
 write.csv(samettpFitsSenate, "samettpModelFitsSenate100.csv")
